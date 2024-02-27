2cbf469d745b6f40f8b36f7d0439c022cab2a622
maxnatalia <Nataliamazur1988@gmail.com>
2024-02-27 08:29:18 +0100

    updated - added options for connectDB

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-const connectDB = (url) => mongoose.connect(url);
+const connectDB = (url) => mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+});
 
 module.exports = connectDB;
