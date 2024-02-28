d64c538c6f2ccaebfdc45e61f7786d85c4d98024
maxnatalia <Nataliamazur1988@gmail.com>
2024-02-28 09:14:00 +0100

    updated - added errors and logic for login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,35 @@
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
-// const bcrypt = require('bcryptjs');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 const User = require('../models/User');
 
 const register = async (req, res) => {
-    const user = await User.create({ ...req.body })
-    res.send("register")
-    res.status(StatusCodes.CREATED).json({ user })
+    const user = await User.create({ ...req.body });
+    const token = user.createJWT();
+
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
-    res.send("login user")
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        throw new BadRequestError("Please provide email and password")
+    }
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new UnauthenticatedError("Invalid crudentials")
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError("Invalid crudentials")
+    }
+
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
